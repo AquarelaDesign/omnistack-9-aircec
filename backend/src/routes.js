117c39ba314fbcafa39fb0bdbe15e68c2b160b99
Jose Augusto <aquarela.design@gmail.com>
2019-10-01 22:39:41 -0300

    Include routes for all services

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,22 @@
 const express = require('express')
+const multer = require('multer')
+const uploadConfig = require('./config/upload')
+
+const SessionController = require('./controllers/SessionController')
+const SpotController = require('./controllers/SpotController')
+const DashboardController = require('./controllers/DashboardController')
+const BookingController = require('./controllers/BookingController')
 
 const routes = express.Router()
+const upload = multer(uploadConfig)
 
-// GET, POST, PUT, DELETE
+routes.post('/sessions', SessionController.store)
 
-// req.query.propriedade  => Acessar Query Params => Para Filtros
-// req.params.propriedade => Acessar Route Params => Para Edição, Exclusão
-// req.body.propriedade   => Acessar Corpo da Requisição => Para Criação, Edição  
+routes.get('/spots', SpotController.index)
+routes.post('/spots', upload.single('thumbnail') , SpotController.store)
 
-routes.get('/', (req, res) => {
-   return res.json({ message: "Hello World!!!"})
-})
+routes.get('/dashboard', DashboardController.show)
+
+routes.post('/spots/:spot_id/bookings', BookingController.store)
 
 module.exports = routes
