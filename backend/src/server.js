1cc1ce484cd73cec0b1367715784439235b52f56
Jose Augusto <aquarela.design@gmail.com>
2019-10-05 17:52:56 -0300

    Socket init

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,20 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const path = require('path')
-
+const socketio = require('socket.io')
+const http = require('http')
 
 const routes = require('./routes')
 
 const app = express()
+const server = http.Server(app)
+const io = socketio(server)
+
+io.on('connection', socket => {
+   console.log('usuario', socket.id)
+   //setTimeout(() => {socket.emit('hello', 'World')}, 4000)
+   
+})
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-3msp6.mongodb.net/semana09?retryWrites=true&w=majority', {
    useNewUrlParser: true,
@@ -18,5 +27,5 @@ app.use(express.json())
 app.use(routes)
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 
-app.listen(3333)
+server.listen(3333)
 
