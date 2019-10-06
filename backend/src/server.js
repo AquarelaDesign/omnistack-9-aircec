fffb9e11d4dc59bf301ffd24a1a097bf55d0ce95
Jose Augusto <aquarela.design@gmail.com>
2019-10-06 01:17:38 -0300

    Omnistack 9 - Completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,15 +11,24 @@ const app = express()
 const server = http.Server(app)
 const io = socketio(server)
 
-io.on('connection', socket => {
-   console.log('usuario', socket.id)
-   //setTimeout(() => {socket.emit('hello', 'World')}, 4000)
-   
-})
-
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-3msp6.mongodb.net/semana09?retryWrites=true&w=majority', {
    useNewUrlParser: true,
    useUnifiedTopology: true,
+})
+
+const connectedUsers = {}
+
+io.on('connection', socket => {
+   const { user_id } = socket.handshake.query
+
+   connectedUsers[user_id] = socket.id
+})
+
+app.use((req, res, next) => {
+   req.io = io
+   req.connectedUsers = connectedUsers
+
+   return next()
 })
 
 app.use(cors())
